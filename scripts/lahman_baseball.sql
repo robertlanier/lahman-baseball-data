472916b8e7b267bea2c1bfa4090d8a648493dc6a
Robert LaNier <45923934+base2bits@users.noreply.github.com>
2021-09-02 19:06:45 -0500

    Working on #6 - RL

--- scripts/lahman_baseball.sql
+++ scripts/lahman_baseball.sql
@@ -65,6 +65,24 @@ ORDER BY total_putouts DESC
 Round the numbers you report to 2 decimal places.
 Do the same for home runs per game. Do you see any trends? */
 
+SELECT 	ROUND(SUM(so)::numeric/SUM(g), 2) AS avg_so_per_g, -- Average strikeouts per game
+		ROUND(SUM(hr)::numeric/SUM(g), 2) AS avg_hr_per_g, -- Average strikeouts per game
+		CASE
+			WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
+			WHEN yearid BETWEEN 1930 AND 1939 THEN '1930s'
+			WHEN yearid BETWEEN 1940 AND 1949 THEN '1940s'
+			WHEN yearid BETWEEN 1950 AND 1959 THEN '1950s'
+			WHEN yearid BETWEEN 1960 AND 1969 THEN '1960s'
+			WHEN yearid BETWEEN 1970 AND 1979 THEN '1970s'
+			WHEN yearid BETWEEN 1980 AND 1989 THEN '1980s'
+			WHEN yearid BETWEEN 1990 AND 1999 THEN '1990s'
+			WHEN yearid BETWEEN 2000 AND 2009 THEN '2000s'
+			WHEN yearid BETWEEN 2010 AND 2019 THEN '2010s'
+			ELSE 'Before 1920s' END AS decades
+FROM teams -- yearly stats and standings
+WHERE yearid >= 1920
+GROUP BY decades
+ORDER BY decades
 
 
 /* 6. Find the player who had the most success stealing bases in 2016, 
